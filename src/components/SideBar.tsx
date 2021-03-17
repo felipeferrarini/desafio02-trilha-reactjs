d835ea2139d4f149e4728d45a92c5c03cabd0313
Felipe de Abreu Ferrarini <ferrarinifelipe@gmail.com>
2021-03-16 23:03:59 -0300

    #Desafio finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
+import { useContext } from 'react';
+import { MovieContext } from '../contexts/MovieContext';
+import { Button } from './Button';
+
 export function SideBar() {
-  // Complete aqui
+  
+  const { genres, handleSelectGenre, selectedGenreId } = useContext(MovieContext);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleSelectGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
---
+++ src/contexts/MovieContext.tsx
@@ -0,0 +1,55 @@
+import { createContext, useEffect, useState } from "react";
+import { GenreResponseProps, MovieProps } from "../@types/projectProps";
+import { api } from "../services/api";
+
+interface MovieContextProps {
+  genres: GenreResponseProps[];
+  handleSelectGenre: (id: number) => void;
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export const MovieContext = createContext({} as MovieContextProps);
+
+export const MovieProvider: React.FC = ({ children }) => {
+
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  function handleSelectGenre(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <MovieContext.Provider value={{
+      genres,
+      handleSelectGenre,
+      selectedGenreId,
+      selectedGenre,
+      movies
+    }}>
+      { children }
+    </MovieContext.Provider>
+  )
+};
